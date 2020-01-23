0b797a2c653d913015c1db33bfc54d6e76b1a86e
unknown <melikaaa98@yahoo.com>
2020-01-23 13:54:06 +0330

    Fixing data base

--- database_create.py
+++ database_create.py
@@ -18,8 +18,8 @@ class Database_create:
           password TEXT NOT NULL,
           genderId INTEGER,
           insuranceId INTEGER, 
-          Foreign Key (genderId) REFERENCES  GENDER(genderId),
-          Foreign Key (insuranceId) REFERENCES  INSURANCE(insuranceId)                
+          Foreign Key (genderId) REFERENCES  GENDER(genderId) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (insuranceId) REFERENCES  INSURANCE(insuranceId) ON DELETE CASCADE ON UPDATE CASCADE               
           )"""
           )
       # conn.commit()
@@ -28,8 +28,8 @@ class Database_create:
           (fuserPhone TEXT,
           suserPhone TEXT,
           id INTEGER PRIMARY KEY AUTOINCREMENT,
-          Foreign Key (fuserPhone) REFERENCES  USER(phone),
-          Foreign Key (suserPhone) REFERENCES  USER(phone)                
+          Foreign Key (fuserPhone) REFERENCES  USER(phone) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (suserPhone) REFERENCES  USER(phone) ON DELETE CASCADE ON UPDATE CASCADE               
           )"""
       )
       
@@ -66,8 +66,8 @@ class Database_create:
           dieseaseId INTEGER NOT NULL,
           discount_percent INTEGER,
           id INTEGER PRIMARY KEY AUTOINCREMENT,
-          Foreign Key (dieseaseId) REFERENCES  DISEASE(dieseaseId),
-          Foreign Key (insuranceId) REFERENCES  INSURANCE(insuranceId)        
+          Foreign Key (dieseaseId) REFERENCES  DISEASE(dieseaseId) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (insuranceId) REFERENCES  INSURANCE(insuranceId) ON DELETE CASCADE ON UPDATE CASCADE       
           )"""  
       )
       # conn.commit()
@@ -86,9 +86,8 @@ class Database_create:
           medical_council_code INTERGER,
           doc_username TEXT,
           id INTEGER PRIMARY KEY AUTOINCREMENT, 
-          Foreign Key (doc_username) REFERENCES  DOCTOR(username),
-          Foreign Key (medical_council_code) REFERENCES  DOCTOR(medical_council_code),
-          Foreign Key (insuranceId) REFERENCES INSURANCE(insuranceId)         
+          Foreign Key (doc_username,medical_council_code) REFERENCES  DOCTOR(username,medical_council_code) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (insuranceId) REFERENCES INSURANCE(insuranceId) ON DELETE CASCADE ON UPDATE CASCADE        
           )"""  
       )
       # conn.commit()
@@ -104,7 +103,7 @@ class Database_create:
           password TEXT NOT NULL,
           specialtyId INTEGER,
           PRIMARY KEY (medical_council_code, username), 
-          Foreign Key (specialtyId) REFERENCES  SPECIALTY(specialtyId)
+          Foreign Key (specialtyId) REFERENCES  SPECIALTY(specialtyId) ON DELETE CASCADE ON UPDATE CASCADE
           )"""  
       )
       # conn.commit()
@@ -124,9 +123,8 @@ class Database_create:
           medical_council_code INTERGER NOT NULL,
           doc_username TEXT,
           id INTEGER PRIMARY KEY AUTOINCREMENT,
-          Foreign Key (doc_username) REFERENCES  DOCTOR(username),
-          Foreign Key (medical_council_code) REFERENCES  DOCTOR(medical_council_code),
-          Foreign Key (phone) REFERENCES  USER(phone)         
+          Foreign Key (doc_username,medical_council_code) REFERENCES  DOCTOR(username,medical_council_code) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (phone) REFERENCES  USER(phone) ON DELETE CASCADE ON UPDATE CASCADE        
           )"""  
       )
       # conn.commit()
@@ -140,12 +138,12 @@ class Database_create:
           doc_username TEXT,
           dhhId INTEGER,
           dohId INTEGER,
-          Foreign Key (dohId) REFERENCES  DOH(dohId),
-          Foreign Key (dhhId) REFERENCES  DHH(dhhId),
-          Foreign Key (doc_username) REFERENCES  DOCTOR(username),
-          Foreign Key (medical_council_code) REFERENCES  DOCTOR(medical_council_code),
-          Foreign Key (phone) REFERENCES  USER(phone), 
-          Foreign Key (paymentId) REFERENCES  PAYMENT(paymentId)    
+          score INTEGER,
+          Foreign Key (dohId) REFERENCES  DOH(dohId) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (dhhId) REFERENCES  DHH(dhhId) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (doc_username,medical_council_code) REFERENCES  DOCTOR(username,medical_council_code) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (phone) REFERENCES  USER(phone) ON DELETE CASCADE ON UPDATE CASCADE, 
+          Foreign Key (paymentId) REFERENCES  PAYMENT(paymentId) ON DELETE CASCADE ON UPDATE CASCADE    
           )"""  
       )
       # conn.commit()
@@ -157,10 +155,9 @@ class Database_create:
           doc_username TEXT,
           appointmentId INTEGER,
           dohId INTEGER PRIMARY KEY AUTOINCREMENT,
-          Foreign Key (appointmentId) REFERENCES  APPOINTMENT(appointmentId),
-          Foreign Key (doc_username) REFERENCES  DOCTOR(username),
-          Foreign Key (medical_council_code) REFERENCES  DOCTOR(medical_council_code),
-          Foreign Key (docofficeId) REFERENCES  DOCTOR_OFFICE(docofficeId) 
+          Foreign Key (appointmentId) REFERENCES  APPOINTMENT(appointmentId) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (doc_username,medical_council_code) REFERENCES  DOCTOR(username,medical_council_code) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (docofficeId) REFERENCES  DOCTOR_OFFICE(docofficeId) ON DELETE CASCADE ON UPDATE CASCADE
           )"""  
       )
       # conn.commit()
@@ -170,7 +167,7 @@ class Database_create:
           (docofficeId INTEGER PRIMARY KEY NOT NULL,
           addressId INTEGER,
           phone INTEGER,
-          Foreign Key (addressId) REFERENCES  ADDRESS(addressId)
+          Foreign Key (addressId) REFERENCES  ADDRESS(addressId) ON DELETE CASCADE ON UPDATE CASCADE
           )"""  
       )
       # conn.commit()
@@ -183,8 +180,8 @@ class Database_create:
           street TEXT,
           alley TEXT,
           plaque TEXT, 
-          Foreign Key (healthcareId) REFERENCES  HEALTHCARE(healthcareId),
-          Foreign Key (docofficeId) REFERENCES  DOCTOR_OFFICE(docofficeId) 
+          Foreign Key (healthcareId) REFERENCES  HEALTHCARE(healthcareId) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (docofficeId) REFERENCES  DOCTOR_OFFICE(docofficeId) ON DELETE CASCADE ON UPDATE CASCADE
           )"""  
       )
       # conn.commit()
@@ -197,8 +194,8 @@ class Database_create:
           date TEXT,
           dhhId INTEGER,
           dohId INTEGER,
-          Foreign Key (dhhId) REFERENCES  DHH(dhhId),
-          Foreign Key (dohId) REFERENCES  DOH(dohId)  
+          Foreign Key (dhhId) REFERENCES  DHH(dhhId) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (dohId) REFERENCES  DOH(dohId) ON DELETE CASCADE ON UPDATE CASCADE 
           )"""  
       )
       # conn.commit()
@@ -210,10 +207,9 @@ class Database_create:
           doc_username TEXT,
           appointmentId INTEGER,
           dhhId INTEGER PRIMARY KEY AUTOINCREMENT,
-          Foreign Key (appointmentId) REFERENCES  APPOINTMENT(appointmentId),
-          Foreign Key (doc_username) REFERENCES  DOCTOR(username),
-          Foreign Key (medical_council_code) REFERENCES  DOCTOR(medical_council_code),
-          Foreign Key (healthcareId) REFERENCES  HEALTHCARE(healthcareId) 
+          Foreign Key (appointmentId) REFERENCES  APPOINTMENT(appointmentId) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (doc_username,medical_council_code) REFERENCES  DOCTOR(username,medical_council_code) ON DELETE CASCADE ON UPDATE CASCADE,
+          Foreign Key (healthcareId) REFERENCES  HEALTHCARE(healthcareId) ON DELETE CASCADE ON UPDATE CASCADE
           )"""  
       )
       # conn.commit()
@@ -224,7 +220,7 @@ class Database_create:
           phone TEXT,
           addressId INTEGER,
           name TEXT,
-          Foreign Key (addressId) REFERENCES ADDRESS(addressId)
+          Foreign Key (addressId) REFERENCES ADDRESS(addressId) ON DELETE CASCADE ON UPDATE CASCADE
 
           )"""  
       )
